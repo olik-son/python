b857def4e2d89cba83e36a3114008097fb26f35c
olik-son <oliwia.ferdyn@gmail.com>
2018-10-27 19:12:37 +0200

    1 wersja

---
+++ praca na konkurs zad1.py
@@ -0,0 +1,41 @@
+from turtle import*
+def atleci(ile):
+  pu();bk(300);pd()
+  for i in range(ile):
+    fillcolor("yellow")
+    fd(40); bk(15)
+    begin_fill()
+    lt(90);fd(10);lt(90);fd(10);lt(90);fd(10)
+    end_fill()
+    fillcolor("green")
+    rt(90);fd(5);lt(90)
+    begin_fill()
+    fd(20);lt(90);fd(20);lt(90);fd(20);lt(90);fd(20)
+    end_fill()
+    lt(90);fd(40);rt(90);fd(10)
+    pu();bk(30);pd();bk(10);fd(10);rt(90);fd(20)
+    fd(20);rt(90);fd(10);pu();bk(40);pd()
+    if i % 2 == 0:
+        #sztanga na dole
+        print("dol")
+        rt(90);fd(20);pu();fd(5);pd();rt(90);fd(20);bk(80);fd(20);lt(90);pu();fd(5);pd();pu();bk(10);pd();bk(20);fd(20);pu();fd(5);pd();lt(90);fd(10);lt(90);fd(5);bk(10);fd(5);fd(5);bk(5);rt(90)
+        #huhyhy
+        fd(5);lt(90);fd(10);bk(20);fd(10);rt(90)
+        fd(5);lt(90);fd(15);bk(30)
+        fd(15);lt(90)
+        fd(80);lt(90);fd(15);bk(30);fd(15);rt(90);bk(5);rt(90);fd(10);bk(10);fd(10);bk(20);fd(10);rt(90);fd(5);lt(90);fd(5);bk(10)
+        pu();fd(30);rt(90);fd(10);fd(90);pd()      
+    else:
+        #sztanga na gore
+        print("gora")
+        lt(90);fd(20);lt(90);fd(20);bk(80);fd(20);lt(90);fd(20);bk(20);rt(90);bk(10)
+        lt(90);fd(5);bk(10);fd(5);lt(90)
+        fd(5);lt(90);fd(10);bk(20);fd(10);rt(90)
+        fd(5);lt(90);fd(15);bk(30)
+        fd(15);lt(90)
+        fd(80);lt(90);fd(15);bk(30);fd(15);rt(90);bk(5);rt(90);fd(10);bk(10);fd(10);bk(20);fd(10);rt(90);fd(5);lt(90);fd(5);bk(10)
+        fd(5);rt(90);fd(10);rt(90);fd(20);lt(90);pu();fd(90);pd()
+#main
+liczba = int(input("Ile Ma byc Atletow: "))
+atleci(liczba)
+
---
+++ praca na konkurs zad2.py
@@ -0,0 +1,24 @@
+def kolejna(liczbapoczatkowa,kolejnaliczba):
+ licznik = 1
+ koniec = 0
+ kolejna =0
+ while koniec < 1 :
+    wynik = liczbapoczatkowa + licznik
+    wynikstr=str(wynik)
+    if (wynikstr.find('3') != 0):
+      kolejna = kolejna + 1
+    if kolejna == kolejnaliczba+1:
+        koniec = 2
+    licznik += 1
+    print("string: ", wynikstr)
+    czytrzy = wynikstr.find('3')
+    print ("czy trzy: ", czytrzy)
+    print ("kolejna: ",kolejna)
+ return wynik
+#main
+a = int(input("Liczba poczatkowa: "))
+b = int(input("Liczba kolejna bez 3: "))
+mojwynik=kolejna(a,b)
+print("----------------------------------------------------")
+print("moj wynik: ",mojwynik)
+print("----------------------------------------------------")
