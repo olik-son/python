6dc22b12f8b50a8fa90297291b479b91e500f4a0
olik-son <oliwia.ferdyn@gmail.com>
2018-10-28 15:39:05 +0100

    ver 2

--- praca na konkurs zad3.py
+++ praca na konkurs zad3.py
@@ -1,25 +1,34 @@
 from turtle import*
 def koslawiec():
+  seth(0)
   fillcolor("orange")
   begin_fill()
   for i in range(4):
     fd(14);rt(90);fd(7);lt(90);fd(7);lt(90);fd(7);rt(90);fd(14);rt(90)
   end_fill()
 def poziomy():
+  seth(0)
   fillcolor("green")
   begin_fill()
   fd(21);lt(90);fd(7);lt(90);fd(21);lt(90);fd(7)
   end_fill()
 def pionowy():
+  seth(90)
   fillcolor("yellow")
   begin_fill()
   fd(21);lt(90);fd(7);lt(90);fd(21);lt(90);fd(7)
   end_fill()
 #main
-koslawiec()
-poziomy()
-koslawiec()
-poziomy()
-koslawiec()
-pionowy()
-    
+#poziomy()
+#pionowy()
+ile=3
+for a in range (ile):
+  print (a)
+  koslawiec()
+  pu();fd(35);rt(90);fd(21);lt(90);bk(7);pd()
+  if a!=ile-1:
+   poziomy()
+   pu();lt(90);fd(14);lt(90);fd(21);pd()
+  
+  
+  
